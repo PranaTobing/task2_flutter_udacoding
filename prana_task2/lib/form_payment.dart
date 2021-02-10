8def08599939d9ebebcfbc8699c950964e0e0775
Prana Tobing <prana.tobing@gmail.com>
2021-02-10 21:55:10 +0700

    membuat app bar dan button

--- prana_task2/lib/form_payment.dart
+++ prana_task2/lib/form_payment.dart
@@ -4,11 +4,28 @@ class FormPayment extends StatelessWidget {
   static String tag = 'form-payment';
   @override
   Widget build(BuildContext context) {
+    final appbar = AppBar(
+      title: Text('Payment Card Demo'),
+      backgroundColor: Colors.deepPurpleAccent[400],
+      centerTitle: true,
+    );
+    final buttonsubmit = RaisedButton(
+      onPressed: null,
+      color: Colors.lightBlueAccent,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(100.0))),
+      padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 80.0),
+      textColor: Colors.white,
+      child: Text('PAY'),
+    );
+    final body = ListView(
+      children: <Widget>[
+        buttonsubmit
+      ],
+    );
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('tes'),
-      ),
+      appBar: appbar,
+      body: body,
     );
   }
 }
