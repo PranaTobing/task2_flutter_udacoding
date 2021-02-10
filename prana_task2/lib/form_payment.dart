a3a40345bf045d3962127e29b1fde7c8c36d1d2b
Prana Tobing <prana.tobing@gmail.com>
2021-02-10 21:16:19 +0700

    add launcher icon image

---
+++ prana_task2/lib/form_payment.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FormPayment extends StatelessWidget {
+  static String tag = 'form-payment';
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('tes'),
+      ),
+    );
+  }
+}
